54c76c86b5e1c25e9b44f9e8f62c7566c7a3edf7
sfondrini <diego.sfondrini@gmail.com>
2024-02-26 21:21:31 +0100

    aggiunta animazioni cards home

--- script/script.js
+++ script/script.js
@@ -8,3 +8,32 @@ window.addEventListener('scroll', () => {
     }
   });
   
+  document.addEventListener("DOMContentLoaded", function() {
+    var cardsWrapper = document.querySelector('.cards-anim');
+    var cards = document.querySelectorAll('.card');
+
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function animateCards() {
+        cards.forEach(function(card, index) {
+            setTimeout(function() {
+                card.classList.add('animated');
+            }, index * 500); // 500 millisecondi = 0.5 secondi
+        });
+    }
+
+    window.addEventListener('scroll', function() {
+        if (isInViewport(cardsWrapper)) {
+            animateCards();
+            window.removeEventListener('scroll', this);
+        }
+    });
+});
