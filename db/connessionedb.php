5e36d02cebb659843e28d717f6d72cc7215a4076
viss13 <visar.hoxha@liceobanfi.eu>
2024-05-27 17:41:17 +0200

    piccolissimo cambio alla connessione del db

--- db/connessionedb.php
+++ db/connessionedb.php
@@ -4,7 +4,7 @@
 	// $db_username = "root";
 	// $db_password = "";
     // $conn = new mysqli($db_servername,$db_username,$db_password,$db_name);
-    $conn = new mysqli("localhost", "root", "","php");
+    $conn = new mysqli("localhost", "root", "","db_php");
     if($conn->connect_error){
         die("<p>Connessione al server non riuscita: ".$conn->connect_error."</p>");
     }
