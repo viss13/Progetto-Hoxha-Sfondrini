e2dcb3a572cf2b9e4d3268b6835153a136f8bad8
sfondrini <diego.sfondrini@gmail.com>
2024-06-02 19:01:02 +0200

    log scheda giocatore

--- pagine/scheda_giocatore.php
+++ pagine/scheda_giocatore.php
@@ -1,33 +1,4 @@
-<?php
-	session_start();
-    if(!isset($_SESSION['username'])){ 
-        echo "devi essere loggato per vedere questa pagina, sarai reindirizzato in 5 secondiels";
-		header('Refresh: 5; URL=login.php');
-	}
-    $username = $_SESSION["username"];
 
-    if (!isset($_GET["id_giocatore"])) {
-        die("Errore! manca un parametro essenziale per il caricamento della pagina!");
-    } else {
-        $id_giocatore = $_GET["id_giocatore"];
-        require("../db/connessionedb.php");
-        $sql = "SELECT giocatori.nome, giocatori.cognome, giocatori.testo, giocatori.copertina, giocatori.soprannome 
-                FROM giocatori
-                WHERE id_giocatore=$id_giocatore"; 
-        $ris = $conn->query($sql) or die("<p>Query fallita!</p>");
-        if ($ris->num_rows == 0) {
-            // decidere che cosa fare
-            die ("giocatore non trovato!");
-        } else {
-            $riga = $ris->fetch_assoc();
-            $testo = $riga['testo'];
-            $nome = $riga["nome"];
-            $cognome = $riga["cognome"];
-            $copertina = $riga["copertina"];
-            $soprannome = $riga["soprannome"];
-        }
-    }
-?>
 
 
 <!DOCTYPE html>
@@ -40,33 +11,69 @@
 	<link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 <body>
-	<?php require("header.php");?>
-	<div class="separatore"></div>
+    <?php
+    if(isset($_SESSION['username'])){
+        session_start();
+        $username = $_SESSION["username"];
+
+        if (!isset($_GET["id_giocatore"])) {
+            die("Errore! manca un parametro essenziale per il caricamento della pagina!");
+        } else {
+            $id_giocatore = $_GET["id_giocatore"];
+            require("../db/connessionedb.php");
+            $sql = "SELECT giocatori.nome, giocatori.cognome, giocatori.testo, giocatori.copertina, giocatori.soprannome 
+                    FROM giocatori
+                    WHERE id_giocatore=$id_giocatore"; 
+            $ris = $conn->query($sql) or die("<p>Query fallita!</p>");
+            if ($ris->num_rows == 0) {
+                // decidere che cosa fare
+                die ("giocatore non trovato!");
+            } else {
+                $riga = $ris->fetch_assoc();
+                $testo = $riga['testo'];
+                $nome = $riga["nome"];
+                $cognome = $riga["cognome"];
+                $copertina = $riga["copertina"];
+                $soprannome = $riga["soprannome"];
+            }
+        }
+        require("header.php");
+	    echo <<< EOD
+        <div class="separatore"></div>
         
         <section class="cover">
             <div class="cover__filter"></div>
 		    <div class="cover__caption">
-                <h1><?php echo "$nome $cognome"?></h1>
+                <h1>$nome $cognome</h1>
                 <h2><?php echo "$soprannome"?></h2>
 		    </div>
-           <?php echo "<img src='../immagini/$copertina' alt='$copertina' class='cover_img'>" ?> 
+           <img src='../immagini/$copertina' alt='$copertina' class='cover_img'>
         </section>
 
         <section class="text hidden-content">
             <div class="text__copy">
-                <?php 
-                    $paragrafi = explode("\n", $testo);
-                    foreach ($paragrafi as $paragrafo) {
-                        echo "<p>$paragrafo</p>";
-                    }
-                ?>
+        EOD;
+
+        $paragrafi = explode("\n", $testo);
+        foreach ($paragrafi as $paragrafo) {
+            echo "<p>$paragrafo</p>";
+        }
+        echo <<< EOD
             </div>
         </section>
+        EOD;
     
-        <?php require("footer.php")?>
+        require("footer.php");
+    }
+    else{
+        echo "<p>devi essere loggato per vedere questa pagina, sarai reindirizzato in 5 secondi</p>";
+		header('Refresh: 5; URL=login.php');
+    }
+    ?>
+	
 
 
-    </div>
+
     <script src="../script/script.js"></script>
     
 </body>
